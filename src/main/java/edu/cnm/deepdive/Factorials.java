3ba619ba7663c69aca2c7a532affa20defba9613
dagoberto <berto35572@gmail.com>
2021-06-28 15:09:14 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/Factorials.java
@@ -0,0 +1,12 @@
+package edu.cnm.deepdive;
+
+public class Factorials {
+
+  public static long computeRecursive(int n) {
+    if (n < 0) {
+      throw new IllegalArgumentException();
+    }
+    return (n == 0) ? 1 : (n * computeRecursive(n-1));
+
+  }
+}
